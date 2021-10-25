6b6ea446d78ed0b7325f0aef258ed254b8310084
Thiago-Cavalcante07 <87835985+Thiago-Cavalcante07@users.noreply.github.com>
2021-10-25 16:10:34 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroCliente/Models/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroCliente.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Pessoa> pessoas { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+
+        }
+    }
+}
